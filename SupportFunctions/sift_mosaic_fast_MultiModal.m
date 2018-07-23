2c359bd7223470ec5048b12d03c1fdf4f1977517
Min Chen <dontminchenit@gmail.com>
2018-07-23 17:16:38 -0400

    Improved Comments and Debug Functionality

--- SupportFunctions/sift_mosaic_fast_MultiModal.m
+++ SupportFunctions/sift_mosaic_fast_MultiModal.m
@@ -1,7 +1,8 @@
 function [bestH, numOkMatches_all, numMatches_all, bestScale]= sift_mosaic_fast_MultiModal(im1, im2, saveDir,saveFlag,f1,d1,f2,d2,TransType,rotLimit,featureType)
 % Matches two images using given precalculated SIFT features
 %Input:
-%im1, im2 -- Input Images to be matched
+%im1 -- Reference Image
+%im2 -- Moving Image to be matched
 %saveMatchesName -- String name if saving the matches to a figure
 %saveFlag -- Set to 1 if you would like to generate figure showing matches
 %f1, f2 -- Vectors of SIFT feature locations locations for im1 and im2
@@ -57,12 +58,12 @@ for m = 1:MN
     total_f1 = total_f1+size(f1{m},2);
     total_f2 = total_f2+size(f2{m},2);
 end
-if(total_f1 < 1 || total_f2 < 1) 
-bestH = eye(3,3);
-numOkMatches_all=0;
-numMatches_all=0;
-bestScale=1;
-return
+if(total_f1 < 1 || total_f2 < 1)
+    bestH = eye(3,3);
+    numOkMatches_all=0;
+    numMatches_all=0;
+    bestScale=1;
+    return
 end
 
 
@@ -82,17 +83,17 @@ for m = 1:MN
     X1_m = f1{m}(1:2,matches_m(1,:)) ;
     X2_m = f2{m}(1:2,matches_m(2,:)) ;
     %check for duplicate matches
-    %check X1 first
-    [uX1_m, IA, IC1] = unique(round(X1_m'),'rows');
+    %check duplicates in X1 & X2 pairs
+    [uX1_m, IA, IC1] = unique([round(X1_m') round(X2_m')],'rows');
     X1_m = X1_m(1:2,IA');
     X2_m = X2_m(1:2,IA');
     matches_m = matches_m(:,IA');
     
     %check X2 next
-    [uX2_m, IA, IC1] = unique(round(X2_m'),'rows');
-    X1_m = X1_m(1:2,IA');
-    X2_m = X2_m(1:2,IA');
-    matches_m = matches_m(:,IA');
+    %    [uX2_m, IA, IC1] = unique(round(X2_m'),'rows');
+    %    X1_m = X1_m(1:2,IA');
+    %    X2_m = X2_m(1:2,IA');
+    %    matches_m = matches_m(:,IA');
     
     matches{m} = matches_m;
     numMatches(m) = size(X1_m,2);
@@ -153,18 +154,18 @@ for t = 1:5000
         Scale = trans.b;
     elseif(TransType == 3)
         %Score using homography (NOT affine)
-%         A = [] ;
-%         for i = subset
-%             A = cat(1, A, kron(X1(:,i)', vl_hat(X2(:,i)))) ;
-%         end
-%         [U,S,V] = svd(A) ;
-%         H = reshape(V(:,9),3,3) ;
-
+        %         A = [] ;
+        %         for i = subset
+        %             A = cat(1, A, kron(X1(:,i)', vl_hat(X2(:,i)))) ;
+        %         end
+        %         [U,S,V] = svd(A) ;
+        %         H = reshape(V(:,9),3,3) ;
+        
         % Use MATLAB function until redoing with proper affine (parallel
         % lines must be preserved!)
         trans = estimateGeometricTransform(X1(1:2,:)', X2(1:2,:)','affine', 'MaxNumTrials',1000,'MaxDistance',matchTolerance);
         H = trans.T';
-       
+        
         TransRadians = atan2(H(1,2),H(1,1));
     elseif(TransType == 4)
         %individual scale
@@ -231,7 +232,13 @@ end
 % --------------------------------------------------------------------
 if(saveFlag)
     %[saveDir,name,ext] = fileparts(saveMatchesName);
-    
+     colorRGB = [51/255 153/255 255/255];
+     f1_ok = f1{1}(1:2,matches{1}(1,bestOK{1}));
+     f2_ok = f2{1}(1:2,matches{1}(2,bestOK{1}));
+     d1_ok = d1{1}(1:2,matches{1}(1,bestOK{1}));
+     d2_ok = d2{1}(1:2,matches{1}(2,bestOK{1}));
+     colorRGB2 = [255/255 153/255 0/255];
+     k=9
     figID=figure(1) ; clf ;
     title(sprintf('%d total tentative matches', numMatches_all)) ;
     title(sprintf('%d (%.2f%%) total inliner matches out of %d', ...
@@ -243,33 +250,40 @@ if(saveFlag)
     for m = 1:MN
         dh1 = max(size(im2{m},1)-size(im1{m},1),0) ;
         dh2 = max(size(im1{m},1)-size(im2{m},1),0) ;
-        
+        %plot all matches
         %subplot(2,MN,m) ;
         figID1 = figure(1);
         gapSize = 25;
         imOut = [padarray(im1{m}(:,:,1),[dh1 gapSize],255,'post') padarray(im2{m}(:,:,1),dh2,255,'post')];
         imagesc(imOut);
-        caxis([min(min(min(im1{m})),min(min(im2{m}))) max(max(max(im1{m})),max(max(im2{m})))]);
+        caxis([min(min(min(im1{m}(:,:,1))),min(min(im2{m}(:,:,1)))) max(max(max(im1{m}(:,:,1))),max(max(im2{m}(:,:,1))))]);
         o = size(im1{m},2) +gapSize;
-        line([f1{m}(1,matches{m}(1,:));f2{m}(1,matches{m}(2,:))+o], ...
-            [f1{m}(2,matches{m}(1,:));f2{m}(2,matches{m}(2,:))]) ;
+        if(numMatches(m) > 0)
+            line([f1{m}(1,matches{m}(1,:));f2{m}(1,matches{m}(2,:))+o], ...
+                [f1{m}(2,matches{m}(1,:));f2{m}(2,matches{m}(2,:))],'color',colorRGB,'LineWidth',2) ;
+           % line([f1_ok(1,k);f2_ok(1,k)+o], ...
+            %    [f1_ok(2,k);f2_ok(2,k)],'color',colorRGB2,'LineWidth',2) ;
+
+        end
         title(sprintf('%d tentative matches', numMatches(m))) ;
         axis image off ;
         colormap('gray')
-        
-        
         set(gca,'LooseInset',get(gca,'TightInset'));
         saveMatchesName=['SIFTmatches_m' num2str(m) '.bmp'];
         saveas(figID1,fullfile(saveDir,saveMatchesName))
         
+        %plot inlier matches
         figID2 = figure(1);
         imOut = [padarray(im1{m}(:,:,1),[dh1 gapSize],255,'post') padarray(im2{m}(:,:,1),dh2,255,'post')];
         imagesc(imOut);
-        caxis([min(min(min(im1{m})),min(min(im2{m}))) max(max(max(im1{m})),max(max(im2{m})))]);
+        caxis([min(min(min(im1{m}(:,:,1))),min(min(im2{m}(:,:,1)))) max(max(max(im1{m}(:,:,1))),max(max(im2{m}(:,:,1))))]);
         o = size(im1{m},2) + gapSize;
         if(max(bestOK{m}) > 0)
             line([f1{m}(1,matches{m}(1,bestOK{m}));f2{m}(1,matches{m}(2,bestOK{m}))+o], ...
-                [f1{m}(2,matches{m}(1,bestOK{m}));f2{m}(2,matches{m}(2,bestOK{m}))]) ;
+                [f1{m}(2,matches{m}(1,bestOK{m}));f2{m}(2,matches{m}(2,bestOK{m}))],'color',colorRGB,'LineWidth',2) ;
+           % line([f1_ok(1,k);f2_ok(1,k)+o], ...
+            %    [f1_ok(2,k);f2_ok(2,k)],'color',colorRGB2,'LineWidth',2) ;
+
         end
         title(sprintf('%d (%.2f%%) inliner matches out of %d', ...
             sum(bestOK{m}), ...
@@ -278,13 +292,66 @@ if(saveFlag)
         axis image off ;
         colormap('gray')
         
-        
-        
         set(gca,'LooseInset',get(gca,'TightInset'));
         saveMatchesName=['RANSACMatches_m' num2str(m) '.bmp'];
         saveas(figID2,fullfile(saveDir,saveMatchesName))
         
-    end
+        %plot select areas
+%         figID3 = figure(1);
+%         imOut = [padarray(im1{m}(:,:,1),[dh1 gapSize],255,'post') padarray(im2{m}(:,:,1),dh2,255,'post')];
+%         imagesc(imOut);
+%         caxis([min(min(min(im1{m})),min(min(im2{m}))) max(max(max(im1{m})),max(max(im2{m})))]);
+%         o = size(im1{m},2) + gapSize;
+%         w = 75;
+%         h = 50;
+%         if(max(bestOK{2}) > 0)
+%             
+%             f1_ok = f1{2}(1:2,matches{2}(1,bestOK{2}));
+%             f2_ok = f2{2}(1:2,matches{2}(2,bestOK{2}));
+%             
+%             for q = [3 10] 1:length(f1_ok)
+%                 q
+%                 if(q == 3)
+%                     colorRGB = [255/255 153/255 0/255];
+%              %   elseif(q == 12)
+%              %       colorRGB = [255/255 0/255 0/255];
+%                 else
+%                   colorRGB = [51/255 153/255 255/255];
+%                 end
+%                 posA = [f1_ok(1,q)-w f1_ok(2,q)-h 2*w+1 2*h+1];
+%                 rectangle('Position',posA,'EdgeColor',colorRGB,'LineWidth',2);
+%                 x_range = round(f1_ok(1,q)-w):round(f1_ok(1,q)+w);
+%                 y_range = round(f1_ok(2,q)-h):round(f1_ok(2,q)+h);
+%                 im_recA = imOut(y_range,x_range);
+%                 posB = [f2_ok(1,q)+o-w f2_ok(2,q)-h 2*w+1 2*h+1];
+%                 rectangle('Position',posB,'EdgeColor',colorRGB,'LineWidth',2);
+%                 x_range = round(f2_ok(1,q)-w+o):round(f2_ok(1,q)+w+o);
+%                 y_range = round(f2_ok(2,q)-h):round(f2_ok(2,q)+h);
+%                 im_recB = imOut(y_range,x_range);
+%                 imwrite(im_recA,fullfile(saveDir,['imA_rect' num2str(q) '_m' num2str(m) '_.tif']))
+%                 imwrite(im_recB,fullfile(saveDir,['imB_rect' num2str(q) '_m' num2str(m) '_.tif']))
+% 
+%                 if (m == 4)
+%                     imA_grid=convertToGrid(im_recA,w,5);
+%                     imB_grid=convertToGrid(im_recB,w,5);
+%                     imwrite(imA_grid,fullfile(saveDir,['imA_rect' num2str(q) '_m' num2str(m) '_grid_.tif']))
+%                     imwrite(imB_grid,fullfile(saveDir,['imB_rect' num2str(q) '_m' num2str(m) '_grid_.tif']))
+%                     figure(5);clf
+%                     imshowpair(imA_grid,imB_grid);
+%                 end                
+%                 %            line([f1{m}(1,matches{m}(1,bestOK{m}));f2{m}(1,matches{m}(2,bestOK{m}))+o], ...
+%                 %                [f1{m}(2,matches{m}(1,bestOK{m}));f2{m}(2,matches{m}(2,bestOK{m}))]) ;
+%             end
+%         end
+%         title('matching areas');
+%         axis image off ;
+%         colormap('gray')
+%         
+%         set(gca,'LooseInset',get(gca,'TightInset'));
+%         saveMatchesName=['MatchedAreas_m' num2str(m) '.bmp'];
+%         saveas(figID2,fullfile(saveDir,saveMatchesName))
+        
+     end
     
     
 end
@@ -297,8 +364,8 @@ if(saveFlag)
     H = bestH;
     for m = 1:MN
         box2 = [1  size(im2{m},2) size(im2{m},2)  1              ;
-                1  1              size(im2{m},1)  size(im2{m},1) ;
-                1  1              1               1            ] ;
+            1  1              size(im2{m},1)  size(im2{m},1) ;
+            1  1              1               1            ] ;
         box2_ = inv(H) * box2 ;
         box2_(1,:) = box2_(1,:) ./ box2_(3,:) ;
         box2_(2,:) = box2_(2,:) ./ box2_(3,:) ;
@@ -306,7 +373,7 @@ if(saveFlag)
         vr = min([1 box2_(2,:)]):max([size(im1{m},1) box2_(2,:)]) ;
         
         [u,v] = meshgrid(ur,vr) ;
-        im1_ = vl_imwbackward(im2double(im1{m}),u,v) ;
+        im1_ = vl_imwbackward(im2double(im1{m}(:,:,1)),u,v) ;
         im1_ = im1_(:,:,1);
         
         saveTif(im1_,saveDir,['image_A_m' num2str(m) '.tif']);
@@ -314,7 +381,7 @@ if(saveFlag)
         z_ = H(3,1) * u + H(3,2) * v + H(3,3);
         u_ = (H(1,1) * u + H(1,2) * v + H(1,3)) ./ z_ ;
         v_ = (H(2,1) * u + H(2,2) * v + H(2,3)) ./ z_ ;
-        im2_ = vl_imwbackward(im2double(im2{m}),u_,v_) ;
+        im2_ = vl_imwbackward(im2double(im2{m}(:,:,1)),u_,v_) ;
         im2_ = im2_(:,:,1);
         
         saveTif(im2_,saveDir,['image_B_Trans_m' num2str(m) '.tif']);
