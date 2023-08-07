7f8f678a80ca9cecad5456724ce4c4fab4109f28
Min Chen <dontminchenit@gmail.com>
2023-08-07 14:13:19 -0400

    Fixed parfor warnings

--- SupportFunctions/calculateFeatures.m
+++ SupportFunctions/calculateFeatures.m
@@ -57,11 +57,11 @@ for n=1:N
                 if(featureType == 0)
                     points = detectSIFTFeatures(im,NumLayersInOctave=5);
                     [features,valid_points] = extractFeatures(im,points);
-                    f1 = valid_points.Location';
-                    d1 = features';
+                    f1p = valid_points.Location';
+                    d1p = features';
 %                    [f1,d1] = vl_sift(im(:,:,1),'Levels',SiftLevel);
                 elseif(featureType == 1)
-                        [f1,d1,Loc_Index,CNNPos] = gridFeatures(im(:,:,1),BPFilterFlags(m),0,[],gridWindowSize,gridBlockSize);%Can't use CNN
+                        [f1p,d1p] = gridFeatures(im(:,:,1),BPFilterFlags(m),0,[],gridWindowSize,gridBlockSize);%Can't use CNN
                         [~,BaseName] = fileparts(imageFilename{m,n});
                         if(saveFeaturesFlag)
                             imageSize = size(im);
@@ -70,10 +70,14 @@ for n=1:N
                             saveConeLoc(SaveName,'CNNPos','imageSize')
                         end
                 else
+                    points = detectSIFTFeatures(im,NumLayersInOctave=5);
+                    [features,valid_points] = extractFeatures(im,points);
+                    f1p = valid_points.Location';
+                    d1p = features';
                     %[f1,d1] = vl_sift(im(:,:,1),'Levels',SiftLevel);
                 end
                 
-                [f1_crop, d1_crop] = filterSiftFeaturesByROI(im, f1, d1, ROICropPct);
+                [f1_crop, d1_crop] = filterSiftFeaturesByROI(im, f1p, d1p, ROICropPct);
                 f_all{m,n} = f1_crop;
                 d_all{m,n} = d1_crop;
             end
